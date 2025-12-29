97548e0d32de2f514acd9376445420f68377f4a4
mahtias <kadjomathias1994@gmail.com>
2025-12-30 03:42:04 +0800

    configuration backend to render

--- backend/dist/lib/prisma.d.ts
+++ backend/dist/lib/prisma.d.ts
@@ -1,11 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 declare const prisma: PrismaClient<{
     log: ("info" | "query" | "warn" | "error")[];
-    datasources: {
-        db: {
-            url: string | undefined;
-        };
-    };
 }, never, import("@prisma/client/runtime/library").DefaultArgs>;
 export default prisma;
 //# sourceMappingURL=prisma.d.ts.map
--- backend/dist/lib/prisma.js
+++ backend/dist/lib/prisma.js
@@ -3,11 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
-    },
 });
 exports.default = prisma;
 //# sourceMappingURL=prisma.js.map
