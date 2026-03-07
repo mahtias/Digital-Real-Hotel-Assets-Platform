191a357a40f4babfc77ae405292aa2dbbd7d3b54
mahtias <kadjomathias1994@gmail.com>
2026-03-07 21:03:37 +0800

    fixed portfolio1

--- frontend/src/pages/Portfolio.jsx
+++ frontend/src/pages/Portfolio.jsx
@@ -46,7 +46,7 @@ export default function Portfolio() {
       .catch(() => setUser(null))
       .finally(() => setUserLoading(false));
   }, [authFetch]);
- console.log("investments state:", investments);
+ //console.log("investments state:", investments);
   // 🔥 Investments
  const { data: investmentsRaw = [], isLoading: investmentsLoading } = useQuery({
   queryKey: ["investments", user?.id],
